3c59ecf6ab9b8268733cdf4016cda204edb8d865
Prapachai U. <prapachai.uea@gmail.com>
2026-08-03 01:08:01 +0700

    Update firebase-config.js
    Change the firebase-rules and set new Firebase

--- firebase-config.js
+++ firebase-config.js
@@ -3,11 +3,11 @@
 // This is safe to commit: it identifies the project, it does not grant access on its own.
 // Access control is enforced entirely by firebase-rules.json (published in the Realtime Database Rules tab).
 export const firebaseConfig = {
-  apiKey: "AIzaSyACYFkxZZIQ6L8027itBO8wtweP8yt7kBo",
-  authDomain: "insider-1d53a.firebaseapp.com",
-  databaseURL: "https://insider-1d53a-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "insider-1d53a",
-  storageBucket: "insider-1d53a.firebasestorage.app",
-  messagingSenderId: "783029865509",
-  appId: "1:783029865509:web:34089f18786490123743cc",
+  apiKey: "AIzaSyAFWtuAA3oWrKMua_MqF_LqYtDMKciDqjc",
+  authDomain: "wavelength-game-323f3.firebaseapp.com",
+  databaseURL: "https://wavelength-game-323f3-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "wavelength-game-323f3",
+  storageBucket: "wavelength-game-323f3.firebasestorage.app",
+  messagingSenderId: "259465241760",
+  appId: "1:259465241760:web:6b45ae3d119b1e9bc955c5"
 };
